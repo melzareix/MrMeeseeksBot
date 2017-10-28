bb9c93d1825062fdbb0ddc541401978ee7bf140c
Mohamed Elzarei <mohamed.elzarei@gmail.com>
2017-10-28 21:53:06 +0200

    refactor: Remove Server Package

--- main.go
+++ main.go
@@ -1,13 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"github.com/joho/godotenv"
-	"github.com/melzareix/MrMeseeksBot/Server"
+	"github.com/melzareix/MrMeseeksBot/Api/Welcome"
 	"github.com/melzareix/MrMeseeksBot/Database"
+	"net/http"
+	"os"
 )
+
+func StartServer() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/welcome", Welcome.Handler)
+	mux.HandleFunc("/chat", nil)
+	mux.HandleFunc("/", nil)
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "3000"
+	}
+	fmt.Println("Server started at http://localhost:" + port)
+	http.ListenAndServe(":"+port, mux)
+}
 
 func main() {
 	godotenv.Load()
 	Database.Init()
-	Server.StartServer()
+	StartServer()
 }
