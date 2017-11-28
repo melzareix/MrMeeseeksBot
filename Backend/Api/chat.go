97733ec45f4fa8eb8f02299b09370f0311fbc37f
Mohamed Elzarei <mohamed.elzarei@gmail.com>
2017-11-28 12:11:44 +0200

    feat: Remove Schedule API HTML link

--- Backend/Api/chat.go
+++ Backend/Api/chat.go
@@ -212,7 +212,7 @@ func HandleScheduling(name string, user *Models.User, w http.ResponseWriter) {
 	resp.Code = http.StatusOK
 	formattedTime := humanize.Time(selectedTime)
 	eventLink = "<a style='color:black' target='_blank' href='" + eventLink + "'>" + eventLink + "</a>"
-	resp.Message = "🕐 Next Episode Airs " + formattedTime + "\n\n" + eventLink
+	resp.Message = "🕐 Next Episode " + formattedTime + "\n" + strip.StripTags(eventLink)
 
 	RespondWithJSON(w, &resp)
 }
